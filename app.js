8fa209c96f115dedd7762680cc63b12f5150b15d
VitaliiSoloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2024-03-21 16:39:20 +0100

    finish

---
+++ app.js
@@ -0,0 +1,49 @@
+const pageSlider = new Swiper('.swiper', {
+	wrapperClass: 'page__wrapper',
+	slidersClass: 'page__screen',
+
+	direction: 'vertical',
+	parallax: 'true',
+	loop: true,
+	speed: 500,
+	watchOverflow: true, // Відключати слайдер. коли вн один
+	slidesPerView: 'auto',
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
+
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+		pageUpDown: true,
+	},
+
+	mousewheel: {
+		sensitivity: 1,
+	},
+
+	pagination: {
+		el: '.page__pagination',
+		type: 'bullets',
+		clickable: true,
+		bulletClass: 'page__bullet',
+		bulletActiveClass: 'page__bullet_active',
+	},
+
+	navigation: {
+		nextEl: '.slider-arrow_next',
+		prevEl: '.slider-arrow_prev',
+	},
+
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+})
+
+/*
+	scrollbar: {
+		el: '.page__scroll',
+		dragClass: 'page__drag-scroll',
+		draggable: true,
+	},
+*/
